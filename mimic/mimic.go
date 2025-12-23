6ecf7905dcf67dedddad1d9aaa31c3ff8ddb2728
anthonyle138 <anthonyle138@gmail.com>
2025-12-23 10:33:16 +0700

    Add Chrome 133 and Safari iOS 18.5 browser profiles
    - Added Chrome 133 profile with updated TLS fingerprint (no post-quantum X25519MLKEM768)
    - Added Safari iOS 18.5 profile (uses Chrome TLS for Shopee compatibility)
    - Chrome 133 is now the default for "chrome" alias
    - Added aliases: chrome133, chrome_133, chrome_legacy, safari, safari_ios, safari_ios_18, safari_ios_185, ios
    
    H2 fingerprints:
    - Chrome 133: 1:65536;2:0;4:6291456;6:262144|15663105||m,a,s,p
    - Safari iOS: Uses Chrome H2 settings for compatibility

--- mimic/mimic.go
+++ mimic/mimic.go
@@ -145,6 +145,99 @@ var (
 		},
 	}
 
+	// Chrome 133 h2 fingerprint: 1:65536;2:0;4:6291456;6:262144|15663105||m,a,s,p
+	// Chrome 133 uses X25519MLKEM768 (post-quantum), ECH, and ALPS
+	chrome133Mimic = Settings{
+		H2HeaderOrder: []string{
+			":method",
+			":authority",
+			":scheme",
+			":path",
+		},
+		H2Settings: []H2Setting{
+			{ID: H2SettingHeaderTableSize, Val: 65536},
+			{ID: H2SettingEnablePush, Val: 0},
+			{ID: H2SettingInitialWindowSize, Val: 6291456},
+			{ID: H2SettingMaxHeaderListSize, Val: 262144},
+		},
+		H2StreamFlow: 15663105,
+		ClientHello: func() *utls.ClientHelloSpec {
+			return &utls.ClientHelloSpec{
+				CipherSuites: []uint16{
+					utls.GREASE_PLACEHOLDER,
+					utls.TLS_AES_128_GCM_SHA256,
+					utls.TLS_AES_256_GCM_SHA384,
+					utls.TLS_CHACHA20_POLY1305_SHA256,
+					utls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+					utls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+					utls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+					utls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					utls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+					utls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+					utls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+					utls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+					utls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+					utls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+					utls.TLS_RSA_WITH_AES_128_CBC_SHA,
+					utls.TLS_RSA_WITH_AES_256_CBC_SHA,
+				},
+				CompressionMethods: []byte{0x00},
+				Extensions: []utls.TLSExtension{
+					&utls.UtlsGREASEExtension{},
+					&utls.SessionTicketExtension{},
+					&utls.SignatureAlgorithmsExtension{SupportedSignatureAlgorithms: []utls.SignatureScheme{
+						utls.ECDSAWithP256AndSHA256,
+						utls.PSSWithSHA256,
+						utls.PKCS1WithSHA256,
+						utls.ECDSAWithP384AndSHA384,
+						utls.PSSWithSHA384,
+						utls.PKCS1WithSHA384,
+						utls.PSSWithSHA512,
+						utls.PKCS1WithSHA512,
+					}},
+					&utls.ApplicationSettingsExtension{SupportedProtocols: []string{"h2"}},
+					&utls.KeyShareExtension{
+						KeyShares: []utls.KeyShare{
+							{Group: utls.CurveID(utls.GREASE_PLACEHOLDER), Data: []byte{0}},
+							{Group: utls.X25519},
+						}},
+					&utls.SCTExtension{},
+					&utls.SupportedPointsExtension{SupportedPoints: []byte{0x00}},
+					&utls.SupportedVersionsExtension{
+						Versions: []uint16{
+							utls.GREASE_PLACEHOLDER,
+							utls.VersionTLS13,
+							utls.VersionTLS12,
+						}},
+					&utls.StatusRequestExtension{},
+					&utls.ALPNExtension{AlpnProtocols: []string{"h2", "http/1.1"}},
+					&utls.SNIExtension{},
+					&utls.UtlsCompressCertExtension{
+						Algorithms: []utls.CertCompressionAlgo{
+							utls.CertCompressionBrotli,
+						}},
+					&utls.SupportedCurvesExtension{
+						Curves: []utls.CurveID{
+							utls.CurveID(utls.GREASE_PLACEHOLDER),
+							utls.X25519,
+							utls.CurveP256,
+							utls.CurveP384,
+						}},
+					&utls.PSKKeyExchangeModesExtension{
+						Modes: []uint8{
+							utls.PskModeDHE,
+						}},
+					&utls.UtlsExtendedMasterSecretExtension{},
+					&utls.RenegotiationInfoExtension{Renegotiation: utls.RenegotiateOnceAsClient},
+					&utls.UtlsGREASEExtension{},
+					&utls.UtlsPaddingExtension{GetPaddingLen: utls.BoringPaddingStyle},
+				},
+				TLSVersMax: utls.VersionTLS13,
+				TLSVersMin: utls.VersionTLS12,
+			}
+		},
+	}
+
 	// Firefox h2 fingerprint: 1:65536;4:131072;5:16384|12517377|3:0:0:201,5:0:0:101,7:0:0:1,9:0:7:1,11:0:3:1,13:0:0:241|m,p,a,s
 	// 3d9132023bf26a71d40fe766e5c24c9d
 	// Firefox tls fingerprint: 771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-156-157-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25,0
@@ -386,11 +479,114 @@ var (
 		},
 	}
 
+	// Safari iOS 18.5 - Uses Chrome TLS fingerprint for Shopee compatibility
+	// Only H2 settings differ slightly
+	safariIOS185Mimic = Settings{
+		H2HeaderOrder: []string{
+			":method",
+			":authority",
+			":scheme",
+			":path",
+		},
+		H2Settings: []H2Setting{
+			{ID: H2SettingHeaderTableSize, Val: 65536},
+			{ID: H2SettingMaxConcurrentStreams, Val: 1000},
+			{ID: H2SettingInitialWindowSize, Val: 6291456},
+			{ID: H2SettingMaxHeaderListSize, Val: 262144},
+		},
+		H2StreamFlow: 15663105,
+		ClientHello: func() *utls.ClientHelloSpec {
+			return &utls.ClientHelloSpec{
+				CipherSuites: []uint16{
+					utls.GREASE_PLACEHOLDER,
+					utls.TLS_AES_128_GCM_SHA256,
+					utls.TLS_AES_256_GCM_SHA384,
+					utls.TLS_CHACHA20_POLY1305_SHA256,
+					utls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+					utls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+					utls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+					utls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					utls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+					utls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+					utls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+					utls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+					utls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+					utls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+					utls.TLS_RSA_WITH_AES_128_CBC_SHA,
+					utls.TLS_RSA_WITH_AES_256_CBC_SHA,
+				},
+				CompressionMethods: []byte{0x00},
+				Extensions: []utls.TLSExtension{
+					&utls.UtlsGREASEExtension{},
+					&utls.SNIExtension{},
+					&utls.UtlsExtendedMasterSecretExtension{},
+					&utls.RenegotiationInfoExtension{Renegotiation: utls.RenegotiateOnceAsClient},
+					&utls.SupportedCurvesExtension{
+						Curves: []utls.CurveID{
+							utls.CurveID(utls.GREASE_PLACEHOLDER),
+							utls.X25519,
+							utls.CurveP256,
+							utls.CurveP384,
+						}},
+					&utls.SupportedPointsExtension{SupportedPoints: []byte{0x00}},
+					&utls.SessionTicketExtension{},
+					&utls.ALPNExtension{AlpnProtocols: []string{"h2", "http/1.1"}},
+					&utls.StatusRequestExtension{},
+					&utls.SignatureAlgorithmsExtension{SupportedSignatureAlgorithms: []utls.SignatureScheme{
+						utls.ECDSAWithP256AndSHA256,
+						utls.PSSWithSHA256,
+						utls.PKCS1WithSHA256,
+						utls.ECDSAWithP384AndSHA384,
+						utls.PSSWithSHA384,
+						utls.PKCS1WithSHA384,
+						utls.PSSWithSHA512,
+						utls.PKCS1WithSHA512,
+					}},
+					&utls.SCTExtension{},
+					&utls.KeyShareExtension{
+						KeyShares: []utls.KeyShare{
+							{Group: utls.CurveID(utls.GREASE_PLACEHOLDER), Data: []byte{0}},
+							{Group: utls.X25519},
+						}},
+					&utls.PSKKeyExchangeModesExtension{
+						Modes: []uint8{
+							utls.PskModeDHE,
+						}},
+					&utls.SupportedVersionsExtension{
+						Versions: []uint16{
+							utls.GREASE_PLACEHOLDER,
+							utls.VersionTLS13,
+							utls.VersionTLS12,
+							utls.VersionTLS11,
+							utls.VersionTLS10,
+						}},
+					&utls.UtlsCompressCertExtension{
+						Algorithms: []utls.CertCompressionAlgo{
+							utls.CertCompressionBrotli,
+						}},
+					&utls.ApplicationSettingsExtension{},
+					&utls.UtlsGREASEExtension{},
+					&utls.UtlsPaddingExtension{GetPaddingLen: utls.BoringPaddingStyle},
+				},
+				TLSVersMax: utls.VersionTLS13,
+				TLSVersMin: utls.VersionTLS10,
+			}
+		},
+	}
+
 	mimicSettingsMap = map[string]Settings{
-		"chrome":     chromeMimic,
-		"chrome83":   chromeMimic,
-		"firefox":    firefoxMimic,
-		"firefox145": firefox145Mimic,
+		"chrome":         chrome133Mimic,
+		"chrome133":      chrome133Mimic,
+		"chrome_133":     chrome133Mimic,
+		"chrome83":       chromeMimic,
+		"chrome_legacy":  chromeMimic,
+		"firefox":        firefoxMimic,
+		"firefox145":     firefox145Mimic,
+		"safari":         safariIOS185Mimic,
+		"safari_ios":     safariIOS185Mimic,
+		"safari_ios_18":  safariIOS185Mimic,
+		"safari_ios_185": safariIOS185Mimic,
+		"ios":            safariIOS185Mimic,
 	}
 )
 
